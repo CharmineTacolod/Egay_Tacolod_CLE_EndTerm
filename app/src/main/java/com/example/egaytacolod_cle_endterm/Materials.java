7a6f532b4757b582e314e550b10984701f759ac9
CharmineTacolod <charminetacolod023@gmail.com>
2023-01-07 19:48:53 +0800

    Egay_Tacolod_CLE_EndTerm

--- app/src/main/java/com/example/egaytacolod_cle_endterm/Materials.java
+++ app/src/main/java/com/example/egaytacolod_cle_endterm/Materials.java
@@ -3,6 +3,15 @@ package com.example.egaytacolod_cle_endterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class Materials extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class Materials extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materials);
+
+        readDataByColumn();
+    }
+
+    ArrayList<String> listDataArray;
+
+    private void readDataByColumn() {
+
+        InputStream is = getResources().openRawResource(R.raw.materials);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+
+        String line = "";
+
+        try {
+            listDataArray = new ArrayList<>();
+
+            while ((line = br.readLine()) != null) {
+
+                String[] cols = line.split(",");
+
+                System.out.println("Column 0 = " + cols[0]);
+                listDataArray.add(cols[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listDataArray);
+        ((ListView) findViewById(R.id.list)).setAdapter(arrayAdapter);
+
     }
 }
